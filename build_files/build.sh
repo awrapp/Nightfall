0c99a94abf64dc16ce277a93bcaaf7820bfb6fe9
Andrew Rapp <awrapp@proton.me>
2026-05-01 00:48:21 -0500

    add kde material you

--- build_files/build.sh
+++ build_files/build.sh
@@ -28,7 +28,11 @@ dnf5 -y copr disable fuddlesworth/PlasmaZones
 ### Klassy for a better default theme
 dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paulmcauley/Fedora_43/home:paulmcauley.repo
 dnf5 -y install klassy
-dnf5 -y config-manager setopt https://download.opensuse.org/repositories/home:paulmcauley/Fedora_43/home:paulmcauley.repo.enabled=0
+#dnf5 -y config-manager setopt https://download.opensuse.org/repositories/home:paulmcauley/Fedora_43/home:paulmcauley.repo.enabled=0
+
+### KDE material you for a better default theme
+dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:luisbocanegra/Fedora_43/home:luisbocanegra.repo
+dnf5 -y install kde-material-you-colors
 
 #### Example for enabling a System Unit File
 
