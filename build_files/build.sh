0188e9d6726c4ab3c1da027bec1301e78743a10d
Andrew Rapp <awrapp@proton.me>
2026-05-01 00:40:54 -0500

    add klassy for better default theme

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,14 +19,20 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-### awrapp >>>
+## awrapp >>
+### PlasmaZones for excellent window management
 dnf5 -y copr enable fuddlesworth/PlasmaZones
 dnf5 -y install plasmazones
 dnf5 -y copr disable fuddlesworth/PlasmaZones
+
+### Klassy for a better default theme
+dnf5 -y config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paulmcauley/Fedora_43/home:paulmcauley.repo
+dnf5 -y install klassy
+dnf5 -y config-manager setopt https://download.opensuse.org/repositories/home:paulmcauley/Fedora_43/home:paulmcauley.repo.enabled=0
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
 
-### awrapp >>>
+## awrapp >>
 #systemctl enable
